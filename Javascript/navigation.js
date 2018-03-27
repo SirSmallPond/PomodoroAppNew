b1d016ac674c636509ab495a6fe2e9d529eeeeed
SirSmallPond <theosakurai@gmail.com>
2018-03-27 21:20:48 +1100

    Can now open 'Study Tips' in navigation

---
+++ Javascript/navigation.js
@@ -0,0 +1,28 @@
+ //Opens and Closes Basic Navigation
+    /* Set the width of the side navigation to 250px */
+
+    function openMainNav() { //Triggered when icon is clicked
+        document.getElementById("mySidenav").style.width = "350px";
+            //document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+  
+          /* Set the width of the side navigation to 0 */
+    function closeMainNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        //document.body.style.backgroundColor = "white";
+    }
+
+
+ //Opens and Closes Study Tips Navigation
+    /* Set the width of the side navigation to 250px */
+
+    function openStudyNav() { //Triggered when icon is clicked
+        document.getElementById("mySideStudynav").style.width = "350px";
+            //document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+  
+          /* Set the width of the side navigation to 0 */
+    function closeStudyNav() {
+        document.getElementById("mySideStudynav").style.width = "0";
+        //document.body.style.backgroundColor = "white";
+    }
